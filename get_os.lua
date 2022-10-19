9d0de5f276e8920dc77e02c23afce899322dfc60
cacao12 <kyle.cooper@coxautoinc.com>
2022-10-19 19:56:17 -0400

    corrected issue with undir being in project dir based on OS

--- get_os.lua
+++ get_os.lua
@@ -3,9 +3,10 @@ local getOSSettings = {}
 local function getVimConfigDir() 
     local home = os.getenv("HOME")
     local unixResult = "~/.config/nvim/lua/vim/"
+    -- TODO there is a bug in the windows result
     local windowsResult = "~/AppData/Local/nvim/lua/vim/"
     if home ~= nil then 
-        return unixResult 
+        return home 
     else 
         return windowsResult
     end
